7b602622b190255cf19b001f9325a92dabffac63
rengas <mr.rengaswamy@gmail.com>
2013-03-01 16:55:50 +0800

    Added some AI

--- AI.js
+++ AI.js
@@ -25,6 +25,40 @@ return Index;
 			
 }
 
+ this.antAttackBob=function()
+ {
+     //get the ants
+     var Ants  = room.getAnts();
+     var distance =0;
+     var distanceFromAntstobob=new Array();
+     var centerPointOfPlayer=player.Intrinsic.centerPoint
+     var specifics={index:0,Dis:0 }
+     for(var i=0;i<Ants.length;i++)
+     {
+      distance=math.getDistanceBetweenTwoPoints(centerPointOfPlayer,Ants[i].centerPoint);
+      distanceFromAntstobob.push(new specifics(i,distance));
+     }
+     //sort the distance in ascending order with the index to know which ant is the shortest
+     distanceFromAntstobob.sort(function(a,b){if (+a.Dis > +b.Dis) return 1;  return -1;});
+     for(var i=0;i<specifics.length;i++)
+     {
+         // if the player centre point is 5 times the centerpoint of ant then attack the player
+        if(Ants[specifics.index].centerPoint*5<=centerPointOfPlayer)
+        {
+            if(Ants[specifics.index].weapons>=1&&Ants[specifics.index].Intrinsic.health>70)
+            {
+                //move to the player
+                pathfinding.objectgo(Ants[specifics.index],player);
+                //attack the player
+            }
+        }
+     }
+
+ }
+ this.moveAntToBob=function(bob,enemy)
+ {
+
+ }
 
 
 this.Action = function(enemies)
