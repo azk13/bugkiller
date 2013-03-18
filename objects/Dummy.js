732ed75595223c1799cbd8d6d54f1ae45fc37266
renga <mr.rengaswamy@gmail.com>
2013-03-18 17:06:46 +0800

    Added dummy object and wtih ant cluster

---
+++ objects/Dummy.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: renga
+ * Date: 3/18/13
+ * Time: 5:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function Dummy(CenterPoint){
+    this.Intrinsic = new Intrinsic(centerPoint, 40, 40);
+
+
+}
