0d295b9eba431fef233a4cd88336fceb061121c5
kraviolle <chuahs.2@gmail.com>
2013-03-20 17:30:46 +0800

    Add in cflag and cpoint for ant

--- objects/Ants.js
+++ objects/Ants.js
@@ -2,4 +2,6 @@ function Ants(centerPoint){
 	this.Intrinsic = new Intrinsic(centerPoint, 40, 40);
 	this.Intrinsic.color = 'black';
 	this.identity = 'ant';
+	this.cflag = false;
+	this.cpoint = new Point(0, 0);
 }
